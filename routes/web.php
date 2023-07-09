89811f80f4dc59d8b97d8ebe81f59b40f70209cd
Supakorn Suwanansin <aonkakbamak@gmail.com>
2023-07-09 15:25:25 +0700

    ชิ้นงานเสร็จสมบูรณ์

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome')
+    ->with('product',Product::all());
+});
+Route::get('/about', function () {
+    return view('front_end.about');
+});
+Route::get('/menu', function () {
+    return view('front_end.menu');
+});
+Route::get('/contact', function () {
+    return view('front_end.contact');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/product/index',[ProductController::class,'index'])->name('pro.index');
+Route::get('admin/product/create',[ProductController::class,'create'])->name('pro.create');
+Route::post('admin/product/insert',[ProductController::class,'insert']);
+Route::get('admin/product/delete/{id}',[ProductController::class,'delete']);
+Route::get('admin/product/edit/{id}',[ProductController::class,'edit']);
+Route::post('admin/product/update/{id}',[ProductController::class,'update']);
